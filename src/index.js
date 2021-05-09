5de4bd2679a6170b7ae0ce8e480a1200a9e64b84
haris <haris_aksalic@hotmail.com>
2021-05-09 19:23:58 +0200

    push

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./index.css";
+import "../src/assets/styles/site.css";
+import "../src/assets/styles/table.css";
+import { render } from "react-dom";
+import App from "./App";
+
+render(<App />, document.getElementById("root"));
